de5fac92d517d517eb342edc36fcfc61d102dbd5
Tai Heng <tai.heng@trubeapp.com>
2015-08-25 10:52:11 +0100

    Added carthage framework target for iOS.

---
+++ MBProgressHUD_iOS/MBProgressHUD_iOS.h
@@ -0,0 +1,19 @@
+//
+//  MBProgressHUD_iOS.h
+//  MBProgressHUD_iOS
+//
+//  Created by Tai Heng on 25/08/2015.
+//  Copyright © 2015 Matej Bukovinski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MBProgressHUD_iOS.
+FOUNDATION_EXPORT double MBProgressHUD_iOSVersionNumber;
+
+//! Project version string for MBProgressHUD_iOS.
+FOUNDATION_EXPORT const unsigned char MBProgressHUD_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MBProgressHUD_iOS/PublicHeader.h>
+
+#import <MBProgressHUD_iOS/MBProgressHUD.h>
